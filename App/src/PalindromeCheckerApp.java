9d66ce9ffed386ebff2631fb49d43c986e1eddb5
tanishqweb <tu2172@srmist.edu.in>
2026-03-02 16:17:38 +0530

    Tanishq Added UC9: Recursive Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,22 +2,11 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    static class Node {
-        char data;
-        Node next;
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    static Node head;
-
     public static void main(String[] args) {
         System.out.println("=====================================");
         System.out.println("      Palindrome Checker App");
         System.out.println("=====================================");
-        System.out.println("UC8: Linked List Based Palindrome Checker");
+        System.out.println("UC9: Recursive Palindrome Checker");
         System.out.println("-------------------------------------");
 
         Scanner sc = new Scanner(System.in);
@@ -26,11 +15,7 @@ public class PalindromeCheckerApp {
 
         String cleaned = input.replaceAll("\\s+", "").toLowerCase();
 
-        for (char c : cleaned.toCharArray()) {
-            append(c);
-        }
-
-        if (isPalindrome(head)) {
+        if (isPalindrome(cleaned, 0, cleaned.length() - 1)) {
             System.out.println("✅ \"" + input + "\" is a palindrome!");
         } else {
             System.out.println("❌ \"" + input + "\" is NOT a palindrome.");
@@ -41,52 +26,11 @@ public class PalindromeCheckerApp {
         sc.close();
     }
 
-    static void append(char data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-        temp.next = newNode;
-    }
-
-    static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null)
+    static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end)
             return true;
-
-        Node slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data)
-                return false;
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-
-    static Node reverse(Node node) {
-        Node prev = null;
-        Node current = node;
-        Node next;
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        return prev;
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+        return isPalindrome(str, start + 1, end - 1);
     }
 }
