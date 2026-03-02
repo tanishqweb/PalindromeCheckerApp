d5bfdba2d7feb876fbdc42703176919604b1f4ae
tanishqweb <tu2172@srmist.edu.in>
2026-03-02 16:01:18 +0530

    Tanishq Added UC7: Deque-Based Optimized Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,44 +1,37 @@
-import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Stack;
-
+import java.util.*;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-
-
         System.out.println("=====================================");
         System.out.println("      Palindrome Checker App");
         System.out.println("=====================================");
-        System.out.println("UC6: Queue + Stack Based Palindrome Check");
+        System.out.println("UC7: Deque-Based Optimized Palindrome Checker");
         System.out.println("-------------------------------------");
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a word or phrase: ");
         String input = sc.nextLine();
+
         String cleaned = input.replaceAll("\\s+", "").toLowerCase();
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < cleaned.length(); i++) {
-            char ch = cleaned.charAt(i);
-            queue.add(ch);   // enqueue
-            stack.push(ch);  // push
+
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char ch : cleaned.toCharArray()) {
+            deque.add(ch);
         }
+
         boolean isPalindrome = true;
-
-
-        while (!queue.isEmpty()) {
-            if (queue.remove() != stack.pop()) {
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
                 isPalindrome = false;
                 break;
             }
         }
+
         if (isPalindrome) {
             System.out.println("✅ \"" + input + "\" is a palindrome!");
         } else {
             System.out.println("❌ \"" + input + "\" is NOT a palindrome.");
         }
-
 
         System.out.println("-------------------------------------");
         System.out.println("Program execution completed.");
