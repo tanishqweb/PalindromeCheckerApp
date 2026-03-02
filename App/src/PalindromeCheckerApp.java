0d2228c957b518fc561f4a423060dd67bd20a19b
tanishqweb <tu2172@srmist.edu.in>
2026-03-02 15:51:56 +0530

    UC6: Queue + Stack Based Palindrome Check

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,27 +1,39 @@
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+
+
         System.out.println("=====================================");
         System.out.println("      Palindrome Checker App");
         System.out.println("=====================================");
-        System.out.println("UC5: Stack-Based Palindrome Checker");
+        System.out.println("UC6: Queue + Stack Based Palindrome Check");
         System.out.println("-------------------------------------");
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a word or phrase: ");
         String input = sc.nextLine();
         String cleaned = input.replaceAll("\\s+", "").toLowerCase();
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < cleaned.length(); i++) {
-            stack.push(cleaned.charAt(i));
+            char ch = cleaned.charAt(i);
+            queue.add(ch);   // enqueue
+            stack.push(ch);  // push
         }
-        String reversed = "";
-        while (!stack.isEmpty()) {
-            reversed += stack.pop();
+        boolean isPalindrome = true;
+
+
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
         }
-        if (cleaned.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("✅ \"" + input + "\" is a palindrome!");
         } else {
             System.out.println("❌ \"" + input + "\" is NOT a palindrome.");
